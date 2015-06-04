31c2abb2ab3e531c53d17b1ec6edae6d87a56955
Jea杨 <jjonline@jjonline.cn>
2015-06-05 07:21:25 +0800

    修复文章页tag背景色--支持无限个tag

--- static/view.js
+++ static/view.js
@@ -175,7 +175,6 @@ $(function () {
                                 $('#comment-'+_postData.pid).append(_newNode);
                             }else {
                                 //全新评论 检测是否为第一条评论 产生评论容器
-                                //<h3 class="comment-header">网友评论<b>（1）</b></h3><div class="article_comment_list"></div>
                                 var _newNode  = [
                                     '<div class="comment dpt_line" style="background:#FBFCE7;">',
                                         '<div class="avatar"><img src="'+_info.tpl+'images/noAvator.jpg"></div>',
